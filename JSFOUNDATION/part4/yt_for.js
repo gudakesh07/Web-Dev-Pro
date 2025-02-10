e37c1aad5ff23e80c670d1fac860ee1a6807de8b
RITU RAJ <riturajgudakesh@gmail.com>
2025-02-10 13:44:07 +0000

    oohoo html

---
+++ JSFOUNDATION/part4/yt_for.js
@@ -0,0 +1,20 @@
+// FOR
+
+for (let i = 0; i < 10; i++) {
+    const element = i;
+    if(element == 5){
+        console.log("5 is the best number")
+    }
+    console.log(element)
+
+}
+
+// console.log(element)
+
+for (let i  = 0; i  <= 10; i ++) {
+    console.log("Outer loop value")
+    for (let j = 0; j <= 10; j++) {
+        console.log("Inner loop value")
+        
+    }    
+}
