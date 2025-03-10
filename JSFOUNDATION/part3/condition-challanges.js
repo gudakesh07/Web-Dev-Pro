f2b4f290839d2bf1345082ee2594eadf6218c104
RITU RAJ <riturajgudakesh@gmail.com>
2025-03-10 15:55:44 +0000

    gg

---
+++ JSFOUNDATION/part3/condition-challanges.js
@@ -0,0 +1,24 @@
+// Checking if a number is greater than another number.
+
+let num1 = 5;
+let num2 = 10;
+
+if (num1 > num2) {
+    console.log("The first number is greater than the second number.")
+    
+} else {
+    console.log("The second number is greater than the first number.")
+}
+
+// Checking if a string is equal to another String:
+
+let username = "John";
+let anotherUsername = "Jo";
+
+if (username == anotherUsername) {
+    console.log("Pick another username, this one is already taken.")
+}else {
+    console.log("Username is available.")
+}
+
+
