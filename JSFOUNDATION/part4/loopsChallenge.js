ac923e5817638f70e07e6dc5881c3955dbb6162d
RITU RAJ <riturajgudakesh@gmail.com>
2025-03-12 20:12:27 +0000

    gg

--- JSFOUNDATION/part4/loopsChallenge.js
+++ JSFOUNDATION/part4/loopsChallenge.js
@@ -2,12 +2,44 @@
 1. Write a 'while' loop that calculates the sum of all numbers from 1 to 5 and stores the result in a variable named "Sum".
 */
 
-let sum = 0
-
-let i = 0
+let sum = 0;
+let i = 1;
 
 while (i <= 5) {
-    sum = sum + 1
-    i = i++
+    sum += i;
+    i++;
 }
 console.log(sum)
+
+/*
+2. Write a 'while' loop that calculates the sum of all numbers from 5 to 1 and store the numbers in an array named "countdown". 
+*/
+
+let countdown = []
+
+let j = 5;
+
+while (j<=5 && j >= 1) {
+    countdown.push(j)
+    j--;
+}
+console.log(countdown)
+
+/* 
+3. Write a 'do while' loop that prompts a user to enter their favourite tea type until they enter '"stop"'.
+    Store each type in an array named 'teaCollection'.
+*/
+
+let teaCollection = []
+
+let tea
+
+do {
+    tea = prompt(`Enter your favourite tea(type "stop" to finish)`)
+
+    if (tea != "stop") {
+        teaCollection.push(tea)
+    }
+        
+} while (tea !== "stop");
+
