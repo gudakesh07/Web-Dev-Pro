b17284b9351c2ceaf9b11dcb3569c41985800a43
RITU RAJ <riturajgudakesh@gmail.com>
2025-03-14 08:53:22 +0000

    gg

--- JSFOUNDATION/Loops Youtube/one.js
+++ JSFOUNDATION/Loops Youtube/one.js
@@ -1,24 +1,56 @@
 // for
 
-// for (let i = 0; i <= 10; i++) {
+// for (let i = 0; i < 10; i++) {
 //     const element = i;
-//     console.log(i);
-//     if (element == 5) {
-//         console.log("5 is the best number")
-//     } else {
-//         console.log("Jkjk")
-//     }
-    
-    
+//     console.log(i)
 // }
 
-// // console.log(i)
+// for (let j = 9; j >0; --j){
+//     const element = j;
+//     if (j == 7) {
+//         console.log("thala for a reason")
+//     }
+//   console.log(j);
+// }
 
-for (let i = 0; i <= 10; i++) {
-    console.log(`outer loop value ${i}`)
-    for (let j = 0; j <= 10; j++) {
-        // console.log(`Inner loop value ${j} and inner loop ${i}`)
-        console.log(i + '*' + j + '=' + i * j )
+// for (let i = 0; i <= 10; i++) {
+//     console.log(`Outer loop ${i}`);
+//     for (let j = 0; j <= 10; j++) {
+//         console.log(`Inner loop ${j} and inner loop ${i}`)
+//     }
+// }
+
+for (let i = 1; i <= 10; i++) {
+    for (let j = 1; j <= 10; j++) {
+      console.log(`${i} * ${j} = ${i * j}`)
     }
+    console.log(`Table of ${i} ends here`)
+    console.log('-------------------')
+}
 
+let myArray = ["Flash", "Arrow", "Superman", "Batman", "Wonder"];
+console.log(myArray.length)
+
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element)
+}
+
+// break and continue
+
+// for (let i = 1; i < 20; i++) {
+//     if (i == 5) {
+//         console.log(`Detected 5`)
+//         break
+//     }
+//      console.log(`Value of i is ${i}`)
+// }
+
+
+for (let i = 1; i < 20; i++) {
+    if (i == 5) {
+        console.log(`Detected 5`)
+        continue
+    }
+     console.log(`Value of i is ${i}`)
 }
---
+++ JSFOUNDATION/Loops Youtube/while.js
@@ -0,0 +1,26 @@
+let index = 0;
+while (index <= 10) {
+    console.log(`value of index is ${index}`)
+    index = index + 2
+}
+
+let myArray = ["Flash", "Arrow", "Superman", "Batman", "Wonder"];
+
+console.log(myArray.length)
+
+let i = 0;
+
+while (i < myArray.length) {
+    console.log(`value is ${myArray[i]}`);
+    i++;
+}
+
+
+// let score = 11;
+let score = 1;
+
+
+do {
+    console.log(`Score is ${score}`)
+    score++;
+} while (score <= 10)
